a3521574135d085d011b022ceb28eb392425109f
yasingul <yasngl46@gmail.com>
2022-09-11 15:54:07 +0300

    Day 2 Add Business Rules  Global Exception Handling

---
+++ src/demoProject/rentACar/Application/Features/BrandsFeatures/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.BrandsFeatures.Commands.CreateBrandFeatures;
+using Application.Features.BrandsFeatures.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.BrandsFeatures.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Brand, CreatedBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+        }
+    }
+}
